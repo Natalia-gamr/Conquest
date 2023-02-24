000c3fc6ad302296bfcd245dd201785904742bd3
Natalia <77733802+Natalia-gamr@users.noreply.github.com>
2023-02-24 12:31:08 +0300

    Update 2023-02-24T09:30:04.077Z

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    let menu = document.querySelector('.header__mobile')
-    let display = menu.style.display
-    display == 'none' ? display = 'block' : display = 'none'; 
-}
-      
