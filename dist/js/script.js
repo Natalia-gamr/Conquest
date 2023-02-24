efce373cd483891a4edf7daa7ea173fbe4a897a5
Natalia <77733802+Natalia-gamr@users.noreply.github.com>
2023-02-24 11:42:00 +0300

    add all

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    let menu = document.querySelector('.header__mobile')
+    let display = menu.style.display
+    display == 'none' ? display = 'block' : display = 'none'; 
+}
+      
